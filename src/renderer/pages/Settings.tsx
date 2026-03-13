7ad3b0115c29c751ef128d99605d8cfe7c7863ff
Tomas Qagz <tomasqagz@gmail.com>
2026-03-13 05:20:58 -0300

    chore: bump version to 0.1.8

--- src/renderer/pages/Settings.tsx
+++ src/renderer/pages/Settings.tsx
@@ -593,7 +593,7 @@ const handleLanguageChange = useCallback((code: string) => {
         <div className="about-grid">
           <div className="about-row">
             <span className="about-label">{t('settings.about_version')}</span>
-            <span className="about-value">0.1.7</span>
+            <span className="about-value">0.1.8</span>
           </div>
           <div className="about-row">
             <span className="about-label">{t('settings.about_engine')}</span>
